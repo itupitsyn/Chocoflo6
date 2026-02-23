993d31954a94e17bec0495d94110933d3eeb1521
root <root@apps.ru>
2026-02-23 19:11:02 +0000

    feat(product): file saving fix

--- lib/actions/edit-product-action.ts
+++ lib/actions/edit-product-action.ts
@@ -11,35 +11,36 @@ export const editProductAction = authActionClient
   .inputSchema(editProductInputSchema)
   .action(async ({ parsedInput: { description, name, images, id } }) => {
     const fileNames: string[] = [];
-    if (images?.length) {
-      const newFiles = images.filter((item) => 'file' in item);
+    const newFiles = images?.filter((item) => 'file' in item) ?? [];
 
-      const result = await saveImagesToFiles(newFiles.map((item) => item.file));
-      result?.forEach((item) => {
-        if (item.status === 'fulfilled' && item.value) {
-          fileNames.push(item.value);
+    const result = await saveImagesToFiles(newFiles.map((item) => item.file));
+    result?.forEach((item) => {
+      if (item.status === 'fulfilled' && item.value) {
+        fileNames.push(item.value);
+      }
+    });
+
+    const oldItem = await prisma.product.findFirst({
+      where: {
+        id,
+      },
+    });
+
+    console.log(oldItem?.images);
+
+    if (oldItem) {
+      const oldFiles = images?.filter((item) => item.uploaded).map((item) => item.src) ?? [];
+      const filesToDelete: string[] = [];
+
+      oldItem.images.forEach((item) => {
+        if (!oldFiles.find((fn) => fn === item)) {
+          filesToDelete.push(item);
+        } else {
+          fileNames.push(item);
         }
       });
 
-      const oldItem = await prisma.product.findFirst({
-        where: {
-          id,
-        },
-      });
-
-      if (oldItem) {
-        const oldFiles = images.filter((item) => item.uploaded).map((item) => item.src);
-        const filesToDelete: string[] = [];
-
-        oldItem.images.forEach((item) => {
-          if (!oldFiles.find((fn) => fn === item)) {
-            filesToDelete.push(item);
-          } else {
-            fileNames.push(item);
-          }
-        });
-        deleteFiles(filesToDelete);
-      }
+      deleteFiles(filesToDelete);
     }
 
     const data = await prisma.product.update({
